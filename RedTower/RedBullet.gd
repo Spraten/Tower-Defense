872eaf01840661fbd37ca2a20c26feeaf79ad89f
Nard <trevod.carter20@gmail.com>
2024-07-21 13:06:37 -0400

    Towers

--- RedTower/RedBullet.gd
+++ RedTower/RedBullet.gd
@@ -1,1 +1,12 @@
 extends CharacterBody2D
+
+
+var target
+var speed = 1000
+var pathName = ""
+var bulletDamage
+
+
+func _physics_process(delta):
+	var pathSpawnerNode = get_tree().get_root()get_node()
+	
