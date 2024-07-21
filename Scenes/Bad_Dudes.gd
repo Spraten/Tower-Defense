7d9bf3cb130452f6caef965117abf9520376d240
Spraten <UncraftedSpraten@gmail.com>
2024-07-21 14:28:33 -0400

    finished basic zombie

--- Scenes/Bad_Dudes.gd
+++ Scenes/Bad_Dudes.gd
@@ -1,15 +1,41 @@
-extends Node2D
+extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export var speed = 1000
+var Health = 10
 
-@export var speed = 10
+func _physics_process(delta):
+	get_parent().set_progress(get_parent().get_progress() + speed*delta)
+	
+	if get_parent().get_progress_ratio() == 1:
+		death()
+		Game.Health -= 1
+		
+	if Health <= 0:
+		death()
+		Game.Gold += 1
+	
+func death():
+	get_parent().get_parent().queue_free()
+
+
+
+
+#extends Node2D
+#@export var speed = 10
+
+
+
+
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#get_parrent is referening to the parrent NODE, in this case, the PathFollow2D in MAIN 
-	get_parent().set_progress(get_parent().get_progress() + speed+delta) 
-	if get_parent().get_progress_ratio() == 1:
-		queue_free()
+#func _process(delta):
+#	#get_parrent is referening to the parrent NODE, in this case, the PathFollow2D in MAIN 
+#	get_parent().set_progress(get_parent().get_progress() + speed+delta) 
+	#if get_parent().get_progress_ratio() == 1:
+#		queue_free()
