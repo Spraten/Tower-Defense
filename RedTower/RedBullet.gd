25ae05365b39a8c888a1ba2418a1abd63cd79ce5
Nard <trevod.carter20@gmail.com>
2024-07-21 14:28:50 -0400

    Nard tower

--- RedTower/RedBullet.gd
+++ RedTower/RedBullet.gd
@@ -10,5 +10,13 @@ var bulletDamage
 func _physics_process(delta):
 	var pathSpawnerNode = get_tree().get_root().get_node("Main/PathSpawner")
 	
-	for i in pathSpawnerNode.get_child
+	for i in pathSpawnerNode.get_child_count():
+		if pathSpawnerNode.get_child(i).name == pathName:
+			target = pathSpawnerNode.get_child(i).get_child(0).global_position
+			
+	velocity = global_position.direction_to(target) *speed
+	
+	look_at(target)
+	move_and_slide()
+	
 	
